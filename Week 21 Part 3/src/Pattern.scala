b4668940f61f70331dc3be0988d82ab0f5bd9769
Alex <115927556+Renchaku@users.noreply.github.com>
2023-02-27 12:58:29 +0000

    Add files via upload

---
+++ Week 21 Part 3/src/Pattern.scala
@@ -0,0 +1,37 @@
+
+
+object Pattern {
+  def main(args: Array[String]): Unit = {
+    var rows, cols = 20
+    val on = 'X'
+    val off = ' '
+    
+    var grid = Array.ofDim[Char](rows, cols)
+    
+    var i, j = 0
+    
+    while (i < rows) {
+      j = 0
+      while (j < cols) {
+        grid(i)(j) = if (i != j) on else off
+        j += 1
+        
+      }
+      i += 1
+    }
+    
+    i = 0
+    
+    while (i < rows) {
+      j = 0
+      while (j < cols) {
+        print(" " + grid(i)(j))
+        j += 1
+        
+      }
+      
+      print("\n")
+      i += 1
+    }
+  }
+}
